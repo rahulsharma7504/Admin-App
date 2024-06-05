2d56bb852fe0d2b3e5f2b67de45ececd7cccdc50
Rahul Sharma <rahul658541@gmail.com>
2024-06-05 14:52:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+ 
+// Connect to MongoDB 
+mongoose.connect('mongodb://127.0.0.1:27017/admin-panel', {
+  useNewUrlParser: true,   
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Database Connected'); 
+})
+.catch((error) => {
+  console.error('Database Connection Error:', error);
+});
+
+const app = express();
+app.use(express.json());
+
+app.use(morgan('dev'));
+app.use(cors()); 
+
+// For userRoutes
+const userRoute = require('./Routes/UserRoute');
+app.use('/users', userRoute);
+//For Admin routes
+const adminRoute = require('./Routes/AdminRoutes');
+app.use('/admin', adminRoute);
+
+const PORT = process.env.PORT || 3000; // Use PORT from environment variable if available, otherwise default to 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
